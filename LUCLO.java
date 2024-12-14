cd8f05d6ca8a72efd8e42534bc4bd501232022b0
M.Mudassir <mudassirm3456@gmail.com>
2024-12-14 15:46:45 +0530

    Add files via upload

---
+++ LUCLO.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+            int a=sc.nextInt();
+            if(a==1)
+            System.out.println(4);
+            else
+            System.out.println(4+(3*(a-1)));
+	}
+}
