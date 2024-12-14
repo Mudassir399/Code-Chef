24de263ccc18bee5cc6e0cad9c4dbc30b48d7dc7
M.Mudassir <mudassirm3456@gmail.com>
2024-12-14 15:53:46 +0530

    Add files via upload

---
+++ CABS.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	for(int i=0;i<t;i++)
+	{
+	    int x=sc.nextInt();
+	    int y=sc.nextInt();
+	    if(x<y)
+	    {
+	        System.out.println("FIRST");
+	    }
+	    else if(x==y)
+	    {
+	        System.out.println("ANY");
+	    }
+	    else
+	    {
+	        System.out.println("SECOND");
+	    }
+	    
+	}
+
+	}
+}
---
+++ DOREWARD.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	for(int i=0;i<t;i++)
+	{
+	    int x=sc.nextInt();
+	    if(x<=3)
+	    {
+	        System.out.println("BRONZE");
+	    }
+	    else if(x<=6)
+	    {
+	        System.out.println("SILVER");
+	    }
+	    else
+	    {
+	        System.out.println("GOLD");
+	    }
+	    
+	}
+
+	}
+}
