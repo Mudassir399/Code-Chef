1e356f5708fe1ee342bb9b10db314ec3fb726972
M.Mudassir <mudassirm3456@gmail.com>
2025-03-03 15:48:37 +0530

    Add files via upload

---
+++ APPLORNG.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+	int A=sc.nextInt();
+	int B=sc.nextInt();
+	if ((A+B)<=x)
+	System.out.println("Yes");
+	else
+	System.out.println("NO");
+	
+
+	}
+}
