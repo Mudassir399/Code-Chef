8df4ae17b6e935c643436e7105296553a9896305
M.Mudassir <mudassirm3456@gmail.com>
2025-03-03 16:10:02 +0530

    Add files via upload

---
+++ WECNITK.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	String x=sc.next();
+	String y="WECNITK";
+	if (x.equals(y))
+	System.out.println("Welcome to Web Club!");
+	else
+	System.out.println("Access denied");
+
+	}
+}
