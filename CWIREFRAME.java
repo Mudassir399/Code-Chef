296324e4a2ab774988319815dcd4dd9265b49e19
M.Mudassir <mudassirm3456@gmail.com>
2025-03-17 16:14:31 +0530

    Add files via upload

---
+++ CWIREFRAME.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    for (int i=0;i<t;i++)
+    {
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int x=sc.nextInt();
+        System.out.println(((2*n)+(2*m))*x);
+    }
+	
+	}
+}
+
